e0ae440472efa14827aa934b01dac3e99645633d
Andrew-Cooperz <Andrew-Cooperz@hotmail.co.uk>
2020-09-30 14:37:09 +0100

    Responsive website

---
+++ js/carouselScript.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    AOS.init();
+    $('.slick').slick({
+        speed: 1000
+    });
+    $('.slick-testimonial').slick({
+        arrows: false,
+        dots: true,
+        speed: 1000
+    });
+});
+
